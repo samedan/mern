3c77b1a30a25f3ede4f5a171621132953640631e
samedan <same.dan@gmail.com>
2018-12-30 12:25:11 +0100

    Finished Backend

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,10 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+// Load Input Validation
+const validateRegisterInput = require("../../validation/register");
+const validateLoginInput = require("../../validation/login");
+
 // Load user model
 var User = require("../../models/User");
 
@@ -18,6 +22,12 @@ router.get("/test", (req, res) => res.json({ msg: "Users works!" }));
 // @desc   Register a user
 // @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
       errors.email = "Email already exists";
@@ -54,6 +64,11 @@ router.post("/register", (req, res) => {
 // @desc   Login user / Returns Token
 // @access Public
 router.post("/login", (req, res) => {
+  const { errors, isValid } = validateLoginInput(req.body);
+  // check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   const email = req.body.email;
   const password = req.body.password;
 
@@ -61,7 +76,8 @@ router.post("/login", (req, res) => {
   User.findOne({ email }).then(user => {
     // Check for User
     if (!user) {
-      return res.status(404).json({ email: "user not found" });
+      errors.email = "user not found";
+      return res.status(404).json(errors);
     }
 
     // Check password
@@ -84,7 +100,8 @@ router.post("/login", (req, res) => {
           }
         );
       } else {
-        return res.status(400).json({ password: "Password incorrect" });
+        errors.password = "password incorect";
+        return res.status(400).json(errors);
       }
     });
   });
